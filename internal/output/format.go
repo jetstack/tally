d2fed3ca3b5121a31f73d1b35d442dc9e0782838
Rob Best <robertbest89@gmail.com>
2022-11-11 17:04:57 +0000

    Remove check output
    It's not really readable.

--- internal/output/format.go
+++ internal/output/format.go
@@ -5,8 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"sort"
-	"strings"
 	"text/tabwriter"
 
 	"github.com/jetstack/tally/internal/types"
@@ -27,9 +25,6 @@ const (
 	// the repositories and their scores.
 	FormatWide Format = "wide"
 
-	// FormatChecks prints the individual check scores for each repository
-	FormatChecks Format = "checks"
-
 	// OutputFormatJSON prints the list of packages in JSON format.
 	FormatJSON Format = "json"
 )
@@ -38,17 +33,15 @@ const (
 var Formats = []Format{
 	FormatShort,
 	FormatWide,
-	FormatChecks,
 	FormatJSON,
 }
 
 type writer func(io.Writer, []types.Result) error
 
 var writerMap = map[Format]writer{
-	FormatShort:  writeShort,
-	FormatWide:   writeWide,
-	FormatChecks: writeChecks,
-	FormatJSON:   writeJSON,
+	FormatShort: writeShort,
+	FormatWide:  writeWide,
+	FormatJSON:  writeJSON,
 }
 
 func writeShort(w io.Writer, results []types.Result) error {
@@ -90,82 +83,6 @@ func writeWide(w io.Writer, results []types.Result) error {
 	}
 
 	return nil
-}
-
-func writeChecks(w io.Writer, results []types.Result) error {
-	var checks []string
-	for _, result := range results {
-		if result.Score == nil {
-			continue
-		}
-		for n := range result.Score.Checks {
-			if contains(checks, n) {
-				continue
-			}
-
-			checks = append(checks, n)
-		}
-	}
-	sort.Strings(checks)
-
-	header := "REPOSITORY\tSCORE"
-	for _, check := range checks {
-		header = header + fmt.Sprintf("\t%s", strings.ToUpper(check))
-	}
-	header = header + "\n"
-
-	tw := tabwriter.NewWriter(w, 0, 0, 1, ' ', 0)
-	defer tw.Flush()
-
-	fmt.Fprintf(tw, header)
-
-	printed := map[string]struct{}{}
-	for _, result := range results {
-		if result.Repository == "" {
-			continue
-		}
-		if _, ok := printed[result.Repository]; ok {
-			continue
-		}
-		var line string
-		if result.Score != nil {
-			line = fmt.Sprintf("%s\t%.1f", result.Repository, result.Score.Score)
-			scores := make([]int, len(checks))
-			for n, score := range result.Score.Checks {
-				for i, check := range checks {
-					if n != check {
-						continue
-					}
-					scores[i] = score
-				}
-			}
-			for _, score := range scores {
-				line = line + fmt.Sprintf("\t%d", score)
-			}
-		} else {
-			line = fmt.Sprintf("%s\t%s", result.Repository, "")
-			for range checks {
-				line = line + "\t "
-			}
-		}
-		line = line + "\n"
-
-		fmt.Fprintf(tw, line)
-
-		printed[result.Repository] = struct{}{}
-	}
-
-	return nil
-}
-
-func contains(values []string, value string) bool {
-	for _, v := range values {
-		if v == value {
-			return true
-		}
-	}
-
-	return false
 }
 
 func writeJSON(w io.Writer, results []types.Result) error {
