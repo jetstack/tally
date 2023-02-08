fd903fa219519b2962101968e8e5e7e2d01ec17b
Rob Best <rob.best@jetstack.io>
2023-02-08 16:34:09 +0000

    Improve progress logging
    Generating scores for lots of repositories can take a long time and it
    wasn't apparent before from the output how many scores needed generating
    or how far through the list you were.
    
    I've added a progress bar that tracks how many repositories we've found
    and where we are in the process of fetching scores from them.
    
    We also print a message that indicates what tally is up to, i.e:
    "Generating score for 'X'".
    
    As part of this I've created a scorecard.Client interface that allows us
    to iterate through different methods for fetching scorecard results
    easily. We try every repository with the first client, then move onto the next
    client after the first has had a go at all the repos.
    
    This should make it simpler to add new sources of scorecard data in
    future.
    
    Doing this work made me realise that our output format returns a result
    for every package+(repository/score) combination, when really what I think we
    want is a result per-score (to avoid multiple results with the same
    score). So I've updated the output format to reflect that.
    
    I updated to Go 1.20 while working on this because I wanted the new
    error wrapping capabilities of errors.Join(). This is because the errors
    from the scorecard generation are difficult to handle, so I want to wrap
    every error as 'not found' so we know we can ignore the failure.

---
+++ internal/scorecard/db_test.go
@@ -0,0 +1,164 @@
+package scorecard
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/jetstack/tally/internal/db"
+	"github.com/jetstack/tally/internal/types"
+)
+
+type mockDBScoreReader struct {
+	getChecks func(context.Context, string) ([]db.Check, error)
+	getScores func(context.Context, ...string) ([]db.Score, error)
+}
+
+func (r *mockDBScoreReader) GetChecks(ctx context.Context, repository string) ([]db.Check, error) {
+	return r.getChecks(ctx, repository)
+}
+
+func (r *mockDBScoreReader) GetScores(ctx context.Context, repositories ...string) ([]db.Score, error) {
+	return r.getScores(ctx, repositories...)
+}
+
+func TestDBClientGetScore(t *testing.T) {
+	type testCase struct {
+		sr        db.ScoreReader
+		wantScore *types.Score
+		wantErr   error
+	}
+	testCases := map[string]func(t *testing.T) *testCase{
+		"should return expected score": func(t *testing.T) *testCase {
+			return &testCase{
+				sr: &mockDBScoreReader{
+					getScores: func(context.Context, ...string) ([]db.Score, error) {
+						return []db.Score{
+							{
+								Score: 7.2,
+							},
+						}, nil
+					},
+					getChecks: func(context.Context, string) ([]db.Check, error) {
+						return []db.Check{
+							{
+								Name:  "foo",
+								Score: 8,
+							},
+							{
+								Name:  "bar",
+								Score: 2,
+							},
+						}, nil
+					},
+				},
+				wantScore: &types.Score{
+					Score: 7.2,
+					Checks: map[string]int{
+						"foo": 8,
+						"bar": 2,
+					},
+				},
+			}
+		},
+		"should return error when there is more than one score for a repository": func(t *testing.T) *testCase {
+			return &testCase{
+				sr: &mockDBScoreReader{
+					getScores: func(context.Context, ...string) ([]db.Score, error) {
+						return []db.Score{
+							{
+								Score: 7.2,
+							},
+							{
+								Score: 3.9,
+							},
+						}, nil
+					},
+					getChecks: func(context.Context, string) ([]db.Check, error) {
+						t.Fatalf("unexpected call to GetChecks")
+						return nil, nil
+					},
+				},
+				wantErr: ErrUnexpectedResponse,
+			}
+		},
+		"should returns errors from GetChecks": func(t *testing.T) *testCase {
+			wantErr := errors.New("check error")
+			return &testCase{
+				sr: &mockDBScoreReader{
+					getScores: func(context.Context, ...string) ([]db.Score, error) {
+						return []db.Score{
+							{
+								Score: 7.2,
+							},
+						}, nil
+					},
+					getChecks: func(context.Context, string) ([]db.Check, error) {
+						return nil, wantErr
+					},
+				},
+				wantErr: wantErr,
+			}
+		},
+		"should returns errors from GetScores": func(t *testing.T) *testCase {
+			wantErr := errors.New("check error")
+			return &testCase{
+				sr: &mockDBScoreReader{
+					getScores: func(context.Context, ...string) ([]db.Score, error) {
+						return nil, wantErr
+					},
+					getChecks: func(context.Context, string) ([]db.Check, error) {
+						t.Fatalf("unexpected call to GetChecks")
+						return nil, nil
+					},
+				},
+				wantErr: wantErr,
+			}
+		},
+		"should return ErrNotFound when GetChecks returns db.ErrNotFound": func(t *testing.T) *testCase {
+			return &testCase{
+				sr: &mockDBScoreReader{
+					getScores: func(context.Context, ...string) ([]db.Score, error) {
+						return []db.Score{
+							{
+								Score: 7.2,
+							},
+						}, nil
+					},
+					getChecks: func(context.Context, string) ([]db.Check, error) {
+						return nil, db.ErrNotFound
+					},
+				},
+				wantErr: ErrNotFound,
+			}
+		},
+		"should return ErrNotFound when GetScores returns db.ErrNotFound": func(t *testing.T) *testCase {
+			return &testCase{
+				sr: &mockDBScoreReader{
+					getScores: func(context.Context, ...string) ([]db.Score, error) {
+						return nil, db.ErrNotFound
+					},
+					getChecks: func(context.Context, string) ([]db.Check, error) {
+						t.Fatalf("unexpected call to GetChecks")
+						return nil, nil
+					},
+				},
+				wantErr: ErrNotFound,
+			}
+		},
+	}
+	for n, setup := range testCases {
+		t.Run(n, func(t *testing.T) {
+			tc := setup(t)
+
+			gotScore, err := NewDBClient(tc.sr).GetScore(context.Background(), "foobar")
+			if !errors.Is(err, tc.wantErr) {
+				t.Fatalf("unexpected error: %s", err)
+			}
+			if diff := cmp.Diff(tc.wantScore, gotScore); diff != "" {
+				t.Errorf("unexpected score:\n%s", diff)
+			}
+		})
+	}
+}
